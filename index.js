9170a5084b6275988ab25e0d8192979f7916e00b
Krzysztof Torończak <k.toronczak.dev@gmail.com>
2024-05-03 19:28:14 +0200

    feature: minor refactor, added carousel swipe animation

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ function switchTheme() {
   setColorTheme(colorTheme);
 }
 
-const colorTheme = localStorage.getItem("colorTheme")
+const colorTheme = localStorage.getItem("colorTheme") ?? "light";
 setColorTheme(colorTheme);
 
 const carousel = document.getElementById("carousel-content");
-const carouselItemsCount = carousel.childElementCount;
+const carouselItems = carousel.children;
+const carouselItemsCount = carousel.children.length;
 const carouselItemWidth = carousel.offsetWidth;
 
 let currentCarouselItem = 0;
@@ -20,16 +21,18 @@ let currentCarouselItem = 0;
 function swipeLeft() {
   if (currentCarouselItem >= 1) {
     currentCarouselItem--;
-    console.log(currentCarouselItem)
     carousel.style.transform = `translateX(-${carouselItemWidth * currentCarouselItem}px)`
+    carouselItems[currentCarouselItem].className = "current-carousel-item"
+    carouselItems[currentCarouselItem + 1].classList.remove("current-carousel-item");
   }
 }
 
 function swipeRight() {
   if (currentCarouselItem < carouselItemsCount - 1) {
     currentCarouselItem++;
-    console.log(currentCarouselItem)
     carousel.style.transform = `translateX(-${carouselItemWidth * currentCarouselItem}px)`
+    carouselItems[currentCarouselItem].className += "current-carousel-item"
+    carouselItems[currentCarouselItem - 1].classList.remove("current-carousel-item");
   }
 }
 
