886c5564e1937785ea83bcc46d4982c76b81565e
Krzysztof Toronczak <k.toronczak.dev@gmail.com>
2024-05-03 02:27:58 +0200

    feature: added technologies carousel

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ function setColorTheme(colorTheme) {
 
 function switchTheme() {
   const colorTheme = localStorage.getItem("colorTheme") === "light" ? "dark" : "light";
-  
   setColorTheme(colorTheme);
 }
 
 const colorTheme = localStorage.getItem("colorTheme")
 setColorTheme(colorTheme);
+
+const carousel = document.getElementById("carousel-content");
+const carouselItemsCount = carousel.childElementCount;
+const carouselItemWidth = carousel.offsetWidth;
+
+let currentCarouselItem = 0;
+
+function swipeLeft() {
+  if (currentCarouselItem >= 1) {
+    currentCarouselItem--;
+    console.log(currentCarouselItem)
+    carousel.style.transform = `translateX(-${carouselItemWidth * currentCarouselItem}px)`
+  }
+}
+
+function swipeRight() {
+  if (currentCarouselItem < carouselItemsCount - 1) {
+    currentCarouselItem++;
+    console.log(currentCarouselItem)
+    carousel.style.transform = `translateX(-${carouselItemWidth * currentCarouselItem}px)`
+  }
+}
+
+
